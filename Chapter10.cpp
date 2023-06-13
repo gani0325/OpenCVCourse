3b43a3b68bc5b7e927473703c06e66f1e36c2ab1
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-14 00:36:15 +0900

    ➕[Feat] 문서의 테두리와 꼭짓점 검출해서 화면에 그리기

--- Chapter10.cpp
+++ Chapter10.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 // #10. Document Scanner
 Mat imgOriginal, imgGray, imgCanny, imgThre, imgBlur, imgErode, imgDil;
+vector<Point> initialPoints;
+
 Mat preProcessing(Mat img) {
 	// cvtColor( input Array,  output Array, flag)
 	// : input Array를 입력받아 flag 에 대한 옵션으로 이미지 색채널을 변경
@@ -32,6 +34,51 @@ Mat preProcessing(Mat img) {
 	return imgDil;
 }
 
+vector<Point> getContours(Mat imgDil) {
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	// findContours(image, mode, method, contours=None, hierarchy=None, offset=None) 
+	// : 외곽선 검출이란 객체의 외곽선 좌표를 모두 추출하는 작업
+	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+	vector<Point> biggest;
+	int maxArea = 0;
+
+	for (int i = 0; i < contours.size(); i++) {
+		int area = contourArea(contours[i]);
+		cout << area << endl;
+
+		string objectType;
+
+		if (area > 1000) {
+			float peri = arcLength(contours[i], true);
+			// approxPolyDP(윤곽선, 근사치 정확도, 폐곡선)
+			// : 윤곽선들의 윤곽점들로 근사해 근사 다각형으로 반환
+			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+
+			if (area > maxArea && conPoly[i].size() == 4) {
+				drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 5);
+				biggest = { conPoly[i][0], conPoly[i][1], conPoly[i][2], conPoly[i][3] };
+				maxArea = area;
+			}
+			// drawContours(image, contours, contourIdx, color, thickness=None, lineType=No)
+			// : 검출한 외곽선을 확인하기 위해 이 함수를 이용하여 외곽선을 화면에 그리기
+			//drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 2);
+			//rectangle(imgOriginal, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+		}
+	}
+	return biggest;
+}
+
+void drawPoints(vector<Point> points, Scalar color) {
+	for (int i = 0; i < points.size(); i++) {
+		circle(imgOriginal, points[i], 30, color, FILLED);
+		putText(imgOriginal, to_string(i), points[i], FONT_HERSHEY_PLAIN, 2, color, 2);
+	}
+}
+
 int main() {
 	string path = "Resources/paper.jpg";
 	imgOriginal = imread(path);
@@ -39,6 +86,10 @@ int main() {
 
 	// Preprocessing
 	imgThre = preProcessing(imgOriginal);
+	// Get contours - Biggest
+	initialPoints = getContours(imgThre);
+	drawPoints(initialPoints, Scalar(0, 0, 255));
+	// Warp
 	imshow("image", imgOriginal);
 	imshow("image Dial", imgDil);
 
