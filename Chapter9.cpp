24b5a3ed036bdca43577b8e30f9867f7f3b6909d
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-13 19:45:28 +0900

    ➕[Feat] Detection 되는 color 로 그림 그리기

--- Chapter9.cpp
+++ Chapter9.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 // #9. Project1 - Virtual Painter
 Mat img;
+vector<vector<int>> newPoints;
+
 ////////////////////  COLOR VALUES ////////////////////////////////
 // hmin, smin, vmin, hmax, smax, vmax
 vector<vector<int>> myColors{ {124,48,117,143,170,255},		// Purple
@@ -18,20 +20,21 @@ vector<vector<int>> myColors{ {124,48,117,143,170,255},		// Purple
 vector<Scalar> myColorValues{ {255,0,255},		// Purple
 							  {0,255,0} };		// Green 	
 
-void getContours(Mat imgDil) {
+Point getContours(Mat imgDil) {
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
 
 	// findContours(image, mode, method, contours=None, hierarchy=None, offset=None) 
 	// : 외곽선 검출이란 객체의 외곽선 좌표를 모두 추출하는 작업
 	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+	Point myPoint(0, 0);
 
 	for (int i = 0; i < contours.size(); i++) {
 		int area = contourArea(contours[i]);
 		cout << area << endl;
 
-		vector<vector<Point>> conPoly(contours.size());
-		vector<Rect> boundRect(contours.size());
 		string objectType;
 
 		if (area > 1000) {
@@ -43,28 +46,19 @@ void getContours(Mat imgDil) {
 
 			cout << conPoly[i].size() << endl;
 			boundRect[i] = boundingRect(conPoly[i]);
-
-			int objCor = (int)conPoly[i].size();
-
-			// 산술 계산을 통해 직사각형 or 정사각형 구분하기
-			if (objCor == 3) { objectType = "Tri"; }
-			else if (objCor == 4)
-			{
-				float aspRatio = (float)boundRect[i].width / (float)boundRect[i].height;
-				cout << aspRatio << endl;
-				if (aspRatio > 0.95 && aspRatio < 1.05) { objectType = "Square"; }
-				else { objectType = "Rect"; }
-			}
-			else if (objCor > 4) { objectType = "Circle"; }
+			myPoint.x = boundRect[i].x + boundRect[i].width / 2;
+			myPoint.y = boundRect[i].y;
 
 			// drawContours(image, contours, contourIdx, color, thickness=None, lineType=No)
 			// : 검출한 외곽선을 확인하기 위해 이 함수를 이용하여 외곽선을 화면에 그리기
 			drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
 		}
 	}
+	return myPoint;
 }
 
-void findColor(Mat img) {
+vector<vector<int>> findColor(Mat img) {
 	Mat imgHSV;
 	cvtColor(img, imgHSV, COLOR_BGR2HSV);
 
@@ -74,7 +68,18 @@ void findColor(Mat img) {
 		Mat mask;
 		inRange(imgHSV, lower, upper, mask);
 		//imshow(to_string(i), mask);
-		getContours(mask);
+		Point myPoint = getContours(mask);
+
+		if (myPoint.x != 0 && myPoint.y != 0) {
+			newPoints.push_back({ myPoint.x, myPoint.y, i });
+		}
+	}
+	return newPoints;
+}
+
+void drawOnCanvas(vector<vector<int>> newPoints, vector<Scalar> myColorValues) {
+	for (int i = 0; i < newPoints.size(); i++) {
+		circle(img, Point(newPoints[i][0], newPoints[i][0]), 10, myColorValues[newPoints[i][2]], FILLED);
 	}
 }
 
@@ -83,7 +88,8 @@ int main() {
 
 	while (true) {
 		cap.read(img);
-		findColor(img);
+		newPoints = findColor(img);
+		drawOnCanvas(newPoints, myColorValues);
 
 		imshow("image", img);
 		waitKey(1);
