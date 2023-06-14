9b181e1e77b16bd53704531c84e1617c053f5ac7
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-14 15:11:03 +0900

    crop 된 부분을 png 로 저장하기

--- Chapter11.cpp
+++ Chapter11.cpp
@@ -32,7 +32,9 @@ void main() {
 
 		for (int i = 0; i < plates.size(); i++) {
 			Mat imgCrop = img(plates[i]);
-			imshow(to_string(i), imgCrop);
+			//imshow(to_string(i), imgCrop);
+			// crop 된 부분을 png 로 저장하기
+			imwrite("Resources/Plates/" + to_string(i) + ".png", imgCrop);
 			rectangle(img, plates[i].tl(), plates[i].br(), Scalar(255, 0, 44));
 		}
 
